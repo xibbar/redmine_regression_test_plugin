d56ad96a8c73e394e07c78442e24aa3cadba77e6
fujioka <fujioka@gw.(none)>
2010-04-15 10:44:26 +0900

    hasmanyの書き方を間違えていた

--- app/models/regression_test_iteration.rb
+++ app/models/regression_test_iteration.rb
@@ -1,5 +1,5 @@
 class RegressionTestIteration < ActiveRecord::Base
-  has_many :regression_test_status,:dependent=>:destroy
+  has_many :regression_test_statuses,:dependent=>:destroy
   belongs_to :project
   validates_presence_of :name,:project_id
   acts_as_list :scope=>:project_id
